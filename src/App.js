e2bab6b29b9bc4e2e9fad2cf38f26bb5eb4314c8
Jenkos <you@example.com>
2024-12-09 10:57:23 -0800

    created a Redux app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { PostsList } from './features/posts/postsList'
+import { AddPostForm } from './features/posts/addPostForm'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <section>
+                <h2>Welcome to the Redux Essentials example app!</h2>
+                <AddPostForm/>
+                <PostsList/>
+              </section>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/app/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from '../features/posts/postsSlice'
+
+export default configureStore({
+  reducer: {
+    posts : postsReducer
+    
+
+    
+  },
+  
+  
+  
+})
+
+
