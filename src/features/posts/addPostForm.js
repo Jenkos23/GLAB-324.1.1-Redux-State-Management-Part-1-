d5664a7f4f5b7c0da3cc09bbe51d11791864db5f
Jenkos <you@example.com>
2024-12-10 22:58:05 -0800

    modified the addpostform

--- src/features/posts/addPostForm.js
+++ src/features/posts/addPostForm.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postsSlice";
 
 export const AddPostForm = ()=>{
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    
+    const [userId, setUserId] = useState('')
+
     const dispatch = useDispatch();
+
+    const users = useSelector(state => state.users)
 
 
     const onTitleChanged = e => setTitle(e.target.value);
     const onContentChanged = e => setContent(e.target.value)
-  
+    const onAuthorChanged = e => setUserId(e.target.value)
+
+
     const onSavePostClicked = ()=> {
         if (title && content) {
+            
             dispatch(
-                postAdded({
+                postAdded(
                     title,
-                    content
-                })
+                    content,
+                    userId
+                )
             )
             setTitle('')
             setContent('')
         }
     }
+
+    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+        {user.name}
+        </option>
+       ))
+    
+    
+    
     return(
         <section>
             <h2>Add a New Post</h2>
@@ -37,6 +54,11 @@ export const AddPostForm = ()=>{
                 onChange={onTitleChanged}
                 />
                 
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+                <option value=""></option>
+                {usersOptions}
+                </select>
                 
                 
                 <label htmlFor="postContent">Content:</label>
@@ -47,7 +69,10 @@ export const AddPostForm = ()=>{
                 onChange={onContentChanged}
 
                 />
-                <button type="button" onClick={onSavePostClicked}> Save Post</button>
+
+                
+                <button type="button" onClick={onSavePostClicked} disabled = {!canSave}>
+                     Save Post</button>
 
                 
                 
