54655add2c294a13424164efd162b984eafeda55
Jenkos <you@example.com>
2024-12-10 22:59:17 -0800

    modified the postslist.js

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
+import { PostAuthor } from "./postAuthor";
+import { TimeAgo } from "./timeAgo";
+import { ReactionButtons } from "./reactionsButtons";
 
 export const PostsList = () => {
-    const posts = useSelector(state => state.posts)
+  const posts = useSelector((state) => state.posts);
 
-    const renderedPosts = posts.map(post =>(
-        <article className="post-excerpt" key ={post.id}>
-            <h3>{post.title}</h3>
-            <p className="post-content">
-                {post.content.substring (0, 100)}
-            </p>
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-        </article>
-    ))
+  const renderedPosts = orderedPosts.map((post) => {
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
 
-    return(
-        <section className="posts-list">
-            <h2>Posts</h2>
-            {renderedPosts}
-        </section>
-    )
-}
+        <ReactionButtons post={post} />
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    );
+  });
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
